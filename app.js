585db027d75d5788f6fe45d99270a725ed76596f
Greg Z <ziebold@gmail.com>
2014-05-11 16:45:52 -0600

    like getFile better

--- app.js
+++ app.js
@@ -13,32 +13,23 @@ var filesDir = 'data/files', names = [],
   getFiles = function(dir) {
     return Q.nfcall(fs.readdir, dir);
   },
-  getLines = function(file) {
+  getFile = function(file) {
     var opts = {
       'flag': 'r',
       'encoding': 'utf8'
     };
     console.log('processing ' + file);
-    return Q.nfcall(fs.readFile, path.join(filesDir, file), opts).then(
-      // fulfilled
-      function(contents) {
-        return contents.split('\n').filter(function(v) {
-          return v !== '';
-        });
-      },
-      // rejected
-      function(err) {
-        console.log(err);
-      }
-    );
+    return Q.nfcall(fs.readFile, path.join(filesDir, file), opts);
   };
 
 getFiles(filesDir).then(function(fileArray) {
   console.log('files to read ' + fileArray);
-  return Q.all(fileArray.map(getLines));
-}).then(function(linesArray) {
-  linesArray.forEach(function(lines) {
-    names = names.concat(lines);
+  return Q.all(fileArray.map(getFile));
+}).then(function(contentsArray) {
+  contentsArray.forEach(function(contents) {
+    names = names.concat(contents.split('\n').filter(function(v) {
+      return v !== '';
+    }));
   });
   console.log('DONE!');
   names.sort().reverse().forEach(function(n) {
