90dc1774b17e61c8e3a7db04020f5d3c46f6eb54
Greg Z <ziebold@gmail.com>
2014-05-11 16:32:04 -0600

    getLines instead for getFile

--- app.js
+++ app.js
@@ -13,23 +13,32 @@ var filesDir = 'data/files', names = [],
   getFiles = function(dir) {
     return Q.nfcall(fs.readdir, dir);
   },
-  getFile = function(file) {
+  getLines = function(file) {
     var opts = {
       'flag': 'r',
       'encoding': 'utf8'
     };
     console.log('processing ' + file);
-    return Q.nfcall(fs.readFile, path.join(filesDir, file), opts);
+    return Q.nfcall(fs.readFile, path.join(filesDir, file), opts).then(
+      // fulfilled
+      function(contents) {
+        return contents.split('\n').filter(function(v) {
+          return v !== '';
+        });
+      },
+      // rejected
+      function(err) {
+        console.log(err);
+      }
+    );
   };
 
 getFiles(filesDir).then(function(fileArray) {
   console.log('files to read ' + fileArray);
-  return Q.all(fileArray.map(getFile));
-}).then(function(contentsArray) {
-  contentsArray.forEach(function(contents) {
-    names = names.concat(contents.split('\n').filter(function(v) {
-      return v !== '';
-    }));
+  return Q.all(fileArray.map(getLines));
+}).then(function(linesArray) {
+  linesArray.forEach(function(lines) {
+    names = names.concat(lines);
   });
   console.log('DONE!');
   names.sort().reverse().forEach(function(n) {
